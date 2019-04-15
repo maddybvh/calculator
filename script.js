a72ef19e32ef19139fca602d3b1368b9546815f1
maddybvh <maddybvh@gmail.com>
2019-04-15 14:54:38 -0400

    operator button logic needs a second pass.

--- script.js
+++ script.js
@@ -1,21 +1,17 @@
 function add (a,b) {
-	return a + b;
+	return Number(a) + Number(b);
 }
 
 function subtract (a, b) {
-	return a - b;
+	return Number(a) - Number(b);
 }
 
 function multiply (a, b) {
-    return a * b;
+    return Number(a) * Number(b);
 }
 
 function divide (a, b) {
-    return a / b;
-}
-
-function power(a , b) {
-	return a ** b;
+    return Number(a) / Number(b);
 }
 
 function operate (operator, a, b){
@@ -28,16 +24,47 @@ function operate (operator, a, b){
             return multiply(a, b);
         case "/": 
             return divide(a, b);
-        case "**": 
-            return power(a, b);
     }
 }
 
-let accumulator = 0;
+var accumulator = 0;
+var currentValue = 0;
 
-//buttons
+function displayAccumulator (accumulator){
+    document.getElementById('screen').innerHTML = `${accumulator}`
+}
+
+function displayCurrentValue (currentValue){
+    document.getElementById('screen').innerHTML = `${currentValue}`
+}
+
+//CLEAR
 document.getElementById("clear").addEventListener("click", function(){
     accumulator = 0;
-    document.getElementById('screen').innerHTML = `${accumulator}`
+    currentValue = 0;
+    displayAccumulator(accumulator)
 });
 
+
+//NUMBER BUTTONS
+const buttons = document.querySelectorAll('.numButton');
+buttons.forEach((button) => {button.addEventListener('click', (e) => {
+    currentValue = button.id;
+    displayCurrentValue(currentValue);
+    console.log(currentValue)
+  });
+});
+
+//OPERATORS
+const ops = document.querySelectorAll('.op');
+ops.forEach((button) => {button.addEventListener('click', (e) => {
+    accumulator = operate(button.id, accumulator, currentValue);
+    displayAccumulator(accumulator);
+});
+})
+
+//EQUALS
+document.getElementById("=").addEventListener("click", function(){
+    displayAccumulator(accumulator);
+    console.log(accumulator)
+});
